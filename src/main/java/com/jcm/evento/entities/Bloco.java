9b0d25eb107b59cf43fae39fdf08e84859d28220
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-04-01 13:42:04 -0300

    projeto inicial, add entidades

---
+++ src/main/java/com/jcm/evento/entities/Bloco.java
@@ -0,0 +1,48 @@
+package com.jcm.evento.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Bloco {
+	
+	private Integer id;
+	private Instant inicio;
+	private Instant fim;
+	
+	public Bloco() {
+	}
+	
+	public Bloco(Integer id, Instant inicio, Instant fim) {
+		super();
+		this.id = id;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Instant inicio) {
+		this.inicio = inicio;
+	}
+
+	public Instant getFim() {
+		return fim;
+	}
+
+	public void setFim(Instant fim) {
+		this.fim = fim;
+	}
+	
+}
